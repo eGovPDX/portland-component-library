1dd4e2f24cff5c6b3edc08dd5d28796591bf3baf
Ken Downey <ken@kendowney.com>
2025-05-28 12:18:42 -0700

    add theme files

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -13,9 +13,11 @@ const preview = {
     themes: {
       default: 'pgov',
       list: [
-        { name: 'PGOV Light', class: 'pgov-theme-enabled', color: '#0050d8' },
-        { name: 'PGOV Dark', class: 'pgov-dark-theme-enabled', color: '#1b1b1b' },
-        { name: 'USWDS Default', class: 'uswds-default-theme-applied', color: '#005ea2' }
+        { name: 'PGOV Light', value: 'pgov', class: 'pgov-theme-enabled', color: '#2378c3' },
+        { name: 'PGOV Dark', value: 'pgov-dark', class: 'pgov-dark-theme-enabled', color: '#1b1b1b' },
+        { name: 'USWDS Default', value: 'uswds-default', class: 'uswds-default-theme-applied', color: '#005ea2' },
+        { name: 'Project Theme Light', value: 'project-light', class: 'project-theme-light-applied', color: '#4a77b4' },
+        { name: 'Project Theme Dark', value: 'project-dark', class: 'project-theme-dark-applied', color: '#4a77b4' }
       ],
     },
 
@@ -32,6 +34,8 @@ const preview = {
         pgov: 'pgov-theme-enabled',
         'pgov-dark': 'pgov-dark-theme-enabled',
         'uswds-default': 'uswds-default-theme-applied',
+        'project-light': 'project-theme-light-applied',
+        'project-dark': 'project-theme-dark-applied',
       },
       defaultTheme: 'pgov',
     }),
