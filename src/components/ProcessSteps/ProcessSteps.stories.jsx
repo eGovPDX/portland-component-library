9443283cc0e7af0d473edc5534711a00025cb4cd
Ken Downey <ken@kendowney.com>
2025-07-21 16:32:21 -0700

    add cta prop, make vertical bar dynamic

--- src/components/ProcessSteps/ProcessSteps.stories.jsx
+++ src/components/ProcessSteps/ProcessSteps.stories.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { ProcessSteps } from './ProcessSteps';
+import { Button } from '../Button/Button';
 
 export default {
   title: 'Components/ProcessSteps',
@@ -37,4 +38,27 @@ Default.args = {
       description: 'When your project is complete, schedule an inspection to ensure it meets safety standards.',
     },
   ],
-}; 
+};
+
+export const WithCTA = Template.bind({});
+WithCTA.args = {
+  title: 'Permit Process',
+  mainDescription: 'The permitting process varies by project. Some, like decks, may be more simple and straightforward. Follow these steps:',
+  steps: [
+    {
+      heading: 'Research',
+      description: 'Identify the type of project and which permits are required. Check zoning rules, environmental factors, and property details to avoid issues.',
+      cta: <Button>Apply</Button>,
+    },
+    {
+      heading: 'Apply',
+      description: 'Submit your application online or by email, pay the intake fee, and confirm that all requirements are met. City staff will review your application. You may need to revise or clarify details based on their feedback.',
+      cta: <Button>Apply</Button>,
+    },
+    {
+      heading: 'Build',
+      description: 'After approval, you can start your project. Follow permit conditions and schedule inspections as needed.',
+      cta: <Button>Apply</Button>,
+    },
+  ],
+};
