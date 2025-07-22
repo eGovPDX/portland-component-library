210ebac666b58bbe612d5174856c17a5a6e25607
Ken Downey <ken@kendowney.com>
2025-07-22 12:22:26 -0700

    add story with links

--- src/components/SummaryBox/SummaryBox.stories.jsx
+++ src/components/SummaryBox/SummaryBox.stories.jsx
@@ -11,6 +11,17 @@ export default {
   },
 };
 
+export const WithoutDescription = {
+  args: {
+    heading: 'Have questions?',
+    children: (
+      <Button variant="primary" endIcon={faArrowRight} style={{ width: 'fit-content' }}>
+        Schedule an appointment
+      </Button>
+    ),
+  },
+};
+
 export const WithButton = {
   args: {
     heading: 'Have questions?',
@@ -23,13 +34,17 @@ export const WithButton = {
   },
 };
 
-export const WithoutDescription = {
+export const WithContent = {
   args: {
     heading: 'Have questions?',
+    description: 'Schedule a time to speak with someone from the Permitting and Development office.',
     children: (
-      <Button variant="primary" endIcon={faArrowRight} style={{ width: 'fit-content' }}>
-        Schedule an appointment
-      </Button>
+      <ul style={{ marginLeft: '1rem' }}>
+        <li>If you are under a winter storm warning, <a href="#">find shelter</a> right away.</li>
+        <li>Sign up for your community’s <a href="#">warning system</a>.</li>
+        <li>Learn the signs of, and basic treatments for, <a href="#">frostbite and hypothermia</a>.</li>
+        <li><a href="#">Gather emergency supplies</a> for your home and your car.</li>
+      </ul>
     ),
   },
-}; 
+};
