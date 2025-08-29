9b496a6cc1cfdd7bbee77b885437f97145368a20
Ken Downey <ken@kendowney.com>
2025-08-29 10:28:41 -0700

    chore(storybook): load global styles and apply PGOV theme via preview.js

---
+++ .storybook/preview.js
@@ -0,0 +1,53 @@
+import { withThemeByClassName } from '@storybook/addon-themes';
+import '../src/styles/index.scss';
+import './storybook.css';
+
+const preview = {
+  parameters: {
+    layout: 'centered',
+    controls: {
+      expanded: true,
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    options: {
+      storySort: {
+        order: [
+          'Introduction',
+          'Using the Design System',
+          'Foundations',
+          'Components',
+          'Patterns',
+          'Guidelines',
+          'Templates',
+          'Design Tokens',
+        ],
+      },
+    },
+    themes: {
+      default: 'pgov',
+      list: [
+        { name: 'PGOV Light', value: 'pgov', class: 'pgov-theme', color: '#2378c3' },
+        { name: 'PGOV Dark', value: 'pgov-dark', class: 'pgov-theme pgov-dark-theme-applied', color: '#1b1b1b' },
+        { name: 'USWDS Default', value: 'uswds-default', class: 'uswds-default-theme-applied', color: '#005ea2' },
+      ],
+    },
+    a11y: {
+      test: 'todo',
+    },
+  },
+  decorators: [
+    withThemeByClassName({
+      themes: {
+        pgov: 'pgov-theme',
+        'pgov-dark': 'pgov-theme pgov-dark-theme-applied',
+        'uswds-default': 'uswds-default-theme-applied',
+      },
+      defaultTheme: 'pgov',
+    }),
+  ],
+};
+
+export default preview;
